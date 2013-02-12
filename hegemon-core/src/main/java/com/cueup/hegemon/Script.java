06fb53e1f7d0905bb4bd221f29e209e23e71cc29
Stefan Praszalowicz <stefan@greplin.com>
2013-02-12 13:48:41 -0800

    Switch to slf4j as our logging facade (from commons-logging)

--- hegemon-core/src/main/java/com/cueup/hegemon/Script.java
+++ hegemon-core/src/main/java/com/cueup/hegemon/Script.java
@@ -22,13 +22,13 @@ import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.NativeJavaClass;
 import org.mozilla.javascript.Scriptable;
 import org.mozilla.javascript.ScriptableObject;
 import org.mozilla.javascript.Wrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Set;
@@ -41,7 +41,7 @@ public class Script {
   /**
    * Logging.
    */
-  private static final Log LOG = LogFactory.getLog(Script.class);
+  private static final Logger LOG = LoggerFactory.getLogger(Script.class);
 
   /**
    * Allows for user defined script location.
